756a775d4711a5e738fe2c3703910388299ad349
raonedaniel <raonedaniel2008@gmail.com>
2019-04-24 20:31:57 -0300

    Front end tela de sessao

--- app/src/main/java/senai/mobile/com/br/cinema/activities/HomeActivity.java
+++ app/src/main/java/senai/mobile/com/br/cinema/activities/HomeActivity.java
@@ -1,15 +1,28 @@
 package senai.mobile.com.br.cinema.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import senai.mobile.com.br.cinema.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(HomeActivity.this, SessaoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/senai/mobile/com/br/cinema/activities/SessaoActivity.java
+++ app/src/main/java/senai/mobile/com/br/cinema/activities/SessaoActivity.java
@@ -1,8 +1,12 @@
 package senai.mobile.com.br.cinema.activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import senai.mobile.com.br.cinema.R;
@@ -11,10 +15,12 @@ public class SessaoActivity extends AppCompatActivity {
 
     Spinner sistemas;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sessao);
+
         sistemas = (Spinner) findViewById(R.id.Lançamento);
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.Tela_de_seções, android.R.layout.simple_spinner_item);
         sistemas.setAdapter(adapter);
