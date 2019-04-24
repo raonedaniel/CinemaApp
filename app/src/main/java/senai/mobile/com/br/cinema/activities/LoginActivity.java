16b59ca9a65afec5192c079b23a6ef26ae583c5f
raonedaniel <vitorcg002@gmail.com>
2019-04-24 19:05:46 -0300

    Firebase e tela de Login

---
+++ app/src/main/java/senai/mobile/com/br/cinema/activities/LoginActivity.java
@@ -0,0 +1,81 @@
+package senai.mobile.com.br.cinema.activities;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import senai.mobile.com.br.cinema.DAO.ConfiguracaoFirebase;
+import senai.mobile.com.br.cinema.R;
+import senai.mobile.com.br.cinema.model.Usuario;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
+    private EditText edtEmailLogin;
+    private EditText edtSenhaLogin;
+    private Button btnLogin;
+    private Usuario usuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edtEmailLogin = (EditText) findViewById(R.id.edtEmail);
+        edtSenhaLogin = (EditText) findViewById(R.id.edtESenha);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!edtEmailLogin.getText().toString().equalsIgnoreCase("") && !edtSenhaLogin.getText().toString().equalsIgnoreCase("")) {
+
+                    usuario = new Usuario();
+
+                    usuario.setEmail(edtEmailLogin.getText().toString());
+                    usuario.setSenha(edtSenhaLogin.getText().toString());
+
+                    validarLogin();
+
+                } else {
+                    Toast.makeText(LoginActivity.this, "Preencha os campos de E-mail e Senha", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+    }
+
+    private void validarLogin() {
+        autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
+        autenticacao.signInWithEmailAndPassword(usuario.getEmail().toString(), usuario.getSenha().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    abrirTelaAdministrador();
+                    Toast.makeText(LoginActivity.this, "Login efetuado com sucesso", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Usuário ou Senha inválidos! Tente novamente", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void abrirTelaAdministrador() {
+        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+
+}
--- app/src/main/java/senai/mobile/com/br/cinema/activities/SessaoActivity.java
+++ app/src/main/java/senai/mobile/com/br/cinema/activities/SessaoActivity.java
@@ -3,6 +3,8 @@ package senai.mobile.com.br.cinema.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import senai.mobile.com.br.cinema.R;
+
 public class SessaoActivity extends AppCompatActivity {
 
     @Override
