41319f188e3a279da3c13038200ff1b19ff9962e
raonedaniel <raonedaniel2008@gmail.com>
2019-04-23 22:17:09 -0300

    Front end tela de sessao implementada

---
+++ app/src/main/java/senai/mobile/com/br/cinema/activities/SessaoActivity.java
@@ -0,0 +1,13 @@
+package senai.mobile.com.br.cinema.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SessaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sessao);
+    }
+}
