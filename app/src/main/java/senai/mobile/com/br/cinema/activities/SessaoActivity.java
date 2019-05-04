e33ccb87aac8e257da59697c668f4f824669e3fa
raonedaniel <raonedaniel2008@gmail.com>
2019-05-03 22:21:29 -0300

    Tela sessao finalizada

--- app/src/main/java/senai/mobile/com/br/cinema/activities/SessaoActivity.java
+++ app/src/main/java/senai/mobile/com/br/cinema/activities/SessaoActivity.java
@@ -2,18 +2,25 @@ package senai.mobile.com.br.cinema.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import senai.mobile.com.br.cinema.R;
 
 public class SessaoActivity extends AppCompatActivity {
 
+
     Spinner sistemas;
+    LinearLayout mainLayout;
+    LinearLayout segundoLayout;
+    LinearLayout terceiroLayout;
+    LinearLayout quartoLayout;
 
 
     @Override
@@ -24,5 +31,28 @@ public class SessaoActivity extends AppCompatActivity {
         sistemas = (Spinner) findViewById(R.id.Lançamento);
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.Tela_de_seções, android.R.layout.simple_spinner_item);
         sistemas.setAdapter(adapter);
+
+        sistemas = (Spinner) findViewById(R.id.Lançamentos);
+        ArrayAdapter adapter1 = ArrayAdapter.createFromResource(this, R.array.Tela_de_seções, android.R.layout.simple_spinner_item);
+        sistemas.setAdapter(adapter);
+
+        sistemas = (Spinner) findViewById(R.id.Aventura);
+        ArrayAdapter adapter2 = ArrayAdapter.createFromResource(this, R.array.Tela_de_seções, android.R.layout.simple_spinner_item);
+        sistemas.setAdapter(adapter);
+
+
+        mainLayout = findViewById(R.id.main_layout);
+        segundoLayout = findViewById(R.id.segundo_layout);
+        terceiroLayout = findViewById(R.id.terceiro_layout);
+        quartoLayout = findViewById(R.id.quarto_layout);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // landscape
+            mainLayout.setOrientation(LinearLayout.HORIZONTAL);
+        }
+        else {
+            // portrait
+            mainLayout.setOrientation(LinearLayout.VERTICAL);
+        }
     }
 }
