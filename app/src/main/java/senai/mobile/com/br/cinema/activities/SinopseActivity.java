61600174642132b4e7e9ddb15dc12e3820f19b4e
Rafaael30 <rg47977@gmail.com>
2019-05-15 21:25:17 -0300

    Tela Sinopse implementada de maneira correta

---
+++ app/src/main/java/senai/mobile/com/br/cinema/activities/SinopseActivity.java
@@ -0,0 +1,15 @@
+package senai.mobile.com.br.cinema.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import senai.mobile.com.br.cinema.R;
+
+public class SinopseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sinopse);
+    }
+}
